a53eec451733360f610cff88f9761c435f49f021
David Reedy Jr <davidrjr.konadave@gmail.com>
2015-12-24 18:36:35 -0600

    Initial commit

---
+++ flexiblejobs.php
@@ -0,0 +1,142 @@
+<?php
+
+require_once 'flexiblejobs.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function flexiblejobs_civicrm_config(&$config) {
+  _flexiblejobs_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function flexiblejobs_civicrm_xmlMenu(&$files) {
+  _flexiblejobs_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function flexiblejobs_civicrm_install() {
+  _flexiblejobs_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function flexiblejobs_civicrm_uninstall() {
+  _flexiblejobs_civix_civicrm_uninstall();
+
+  CRM_Core_DAO::executeQuery("DELETE FROM civicrm_setting WHERE group_name='com.klangsoft.flexiblejobs'");
+
+  CRM_Core_DAO::executeQuery("UPDATE civicrm_job SET run_frequency=NULL, is_active=0 WHERE run_frequency IN ('Yearly', 'Quarter', 'Monthly', 'Weekly')");
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function flexiblejobs_civicrm_enable() {
+  _flexiblejobs_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function flexiblejobs_civicrm_disable() {
+  _flexiblejobs_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed
+ *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function flexiblejobs_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _flexiblejobs_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function flexiblejobs_civicrm_managed(&$entities) {
+  _flexiblejobs_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function flexiblejobs_civicrm_caseTypes(&$caseTypes) {
+  _flexiblejobs_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function flexiblejobs_civicrm_angularModules(&$angularModules) {
+_flexiblejobs_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function flexiblejobs_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _flexiblejobs_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Functions below this ship commented out. Uncomment as required.
+ *
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function flexiblejobs_civicrm_preProcess($formName, &$form) {
+
+}
+
+*/
